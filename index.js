0c4746a60b21e87f41765fbb6d55294cf979c606
Pierre RICARD <93609658+Pierre747@users.noreply.github.com>
2022-10-01 11:32:42 +0200

    Feat: responsive update

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+//! Mes constantes
+
 const form = document.querySelector('form'); // Je récupère mon elément formulaire
 
-const trash = new Audio('./assets/sounds/trash.mp3');
+const trash = new Audio('./assets/sounds/trash.mp3'); // Ajout du son
+
+//! Je prépare une boite de stockage pour mes données
 
 const storage = () => {
 	window.localStorage.todolist = list.innerHTML; // window.localStorage.LeNomQueJeVeux = CeQueJeVeuxSauvegarder
 };
 
-// utilisation du localStorage
+//! utilisation du localStorage
 
 const useStorage = () => {
 	if (window.localStorage.todolist) {
@@ -19,7 +23,7 @@ const useStorage = () => {
 
 window.addEventListener('load', useStorage); // Une fois que la page est chargée, prends les données en mémoire
 
-// Ajouter un elément
+//! Ajouter un elément
 
 form.addEventListener('submit', (e) => {
 	e.preventDefault(); // el famoso e.preventDefault pour éviter le chargement de la page au submit
@@ -28,12 +32,12 @@ form.addEventListener('submit', (e) => {
 	storage(); // Sauvegarde mes values dans mon local storage
 });
 
-// Checker un élément avant de le virer
+//! Checker un élément avant de le virer
 
 list.addEventListener('click', (e) => {
 	if (e.target.classList.contains('checked')) {
 		e.target.remove(); // e.target pour sélectionner un élément au click et remove() pour le supprimer https://developer.mozilla.org/en-US/docs/Web/API/Element/remove
-		trash.play();
+		trash.play(); // jouer le son de suppression
 		storage(); // Sauvegarde mes values dans mon local storage
 	} else {
 		e.target.classList.add('checked'); // Ajout de la classe checked pour le style
